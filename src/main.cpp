b2d6a088ab70f9e0f1a67ed8b16893398fdb67c8
Peter Dorich <dorich.peter@gmail.com>
2021-06-28 15:45:44 -0700

    fixed spacing issue with keymap

--- src/main.cpp
+++ src/main.cpp
@@ -20,9 +20,9 @@ using namespace std;
 uint8_t keymap[16] = {
     SDLK_1, SDLK_2, SDLK_3,
     SDLK_q, SDLK_w, SDLK_e,
-	SDLK_a, SDLK_s, SDLK_d,
+    SDLK_a, SDLK_s, SDLK_d,
     SDLK_z, SDLK_x, SDLK_c,
-    SDLK_4, SDLK_r,	SDLK_f,
+    SDLK_4, SDLK_r, SDLK_f,
     SDLK_v,
 };
 
